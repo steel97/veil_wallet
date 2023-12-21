cfdc01bcf855e6bb08a2af22f530b1bc8e8430f6
Ivan Yv <steel-97@mail.ru>
2023-12-21 08:02:58 +0300

    Remove biometrics from MacOS

--- lib/src/core/func_checker.dart
+++ lib/src/core/func_checker.dart
@@ -3,14 +3,13 @@ import 'package:local_auth/local_auth.dart';
 
 var auth = LocalAuthentication();
 
-Future<bool> checkBiometricsSupport()  async{
-  if(Platform.isLinux || Platform.isFuchsia) {
+Future<bool> checkBiometricsSupport() async {
+  if (Platform.isLinux || Platform.isFuchsia || Platform.isMacOS) {
     return false;
   }
-  
-  final bool canAuthenticateWithBiometrics =
-    await auth.canCheckBiometrics;
-  final bool canAuthenticate = canAuthenticateWithBiometrics ||
-    await auth.isDeviceSupported();
+
+  final bool canAuthenticateWithBiometrics = await auth.canCheckBiometrics;
+  final bool canAuthenticate =
+      canAuthenticateWithBiometrics || await auth.isDeviceSupported();
   return canAuthenticate;
 }
